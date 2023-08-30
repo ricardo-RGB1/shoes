45cc037290c4eda742b31c89e5295eedd1a72f24
garcia718gt4 <58898368+garcia718gt4@users.noreply.github.com>
2023-08-29 21:12:02 -0700

    sections

---
+++ src/sections/index.js
@@ -0,0 +1,9 @@
+// import all the .jsx files in the sections folder such as Hero.jsx from './Hero'
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
